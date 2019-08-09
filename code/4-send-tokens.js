6cab96fbc0b178665f6ba7e2484f17459b5f83fd
Jeremy Ellis <keyfreemusic@gmail.com>
2019-08-08 22:55:04 -0700

    Update 4-send-tokens.js

--- code/4-send-tokens.js
+++ code/4-send-tokens.js
@@ -12,30 +12,49 @@ const iota = iotaLibrary.composeAPI({
 const seed =
   'DONOTSTOREYOURSEEDONAPUBLICGITHUBSITEASANYONECANSTEALALLYOURIOTAKEEPYOURSEEDSAFE9'
 
+const converter = require('@iota/converter')
+
+const message = "Sending 6i hope that works"
+
+const message_in_trytes = converter.asciiToTrytes(message)
+
+
 // Create a wrapping function so we can use async/await
 const main = async () => {
+  // Generate a different address from your seed
+ // const receivingAddress = await iota.getNewAddress(seed, {
+ //   index: 1,
+ //   total: 1
+ // })
+
   const receivingAddress = await iota.getNewAddress(seed);
   // Construct a TX to our new address
   const transfers = [
     {
-      value: 5,    // try 0 if you don't yet have confirmed tokens
+      value: 6,
       address: receivingAddress,
+      message : message_in_trytes,
       tag: 'MYMAGIC'
     }
   ]
-  console.log('Sending 5i to ' + receivingAddress)
+  console.log('Sending 6i to ' + receivingAddress)
 
   try {
     // Construct bundle and convert to trytes
     const trytes = await iota.prepareTransfers(seed, transfers)
     // Send bundle to node.
-    const response = await iota.sendTrytes(trytes, 4, 14)    // 4, 14 needed for real IOTA  3, 9 for Devnet not real
+    const response = await iota.sendTrytes(trytes, 4, 14)
 
     console.log('Completed TXs')
-    response.map(tx => console.log(tx))
+    response.map(tx => {
+        console.log(tx);
+          global.myResponse4 = '<h2>4-send-tokens.js</h2> Transaction Completed <br>' + '<pre id="myPre01">'+JSON.stringify(tx, null, 3)+'</pre>' + '<hr>';  // hopefully this is global
+          console.log("4-send-tokens is finished!")
+    })
   } catch (e) {
     console.log(e)
   }
 }
 
 main()
+
